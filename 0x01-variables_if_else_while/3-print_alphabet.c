a2ccda244f2705b692268f2e9d1c39323b73c736
blandine <blandine@gmail.com>
2023-02-20 07:20:21 -0800

    3-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+		int i;
+	char alp[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int x;
+
+	for (i - 0; i < 26; i++)
+		{
+                	putchar(alp[i]);
+		}
+	for (x - o; x < 26; x++)
+		{
+			putchar(alp[i]);
+		}
+        putchar('\n');
+        return (0);
+}
