b4a4e930e2e9439faac289e8f51f7737301f6e3d
blandine <blandine@gmail.com>
2023-02-21 05:30:04 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - print the alphabet in lower case x10
+ *
+ * Return: nothing on success.
+ */
+void print_alphabet_x10(void)
+{
+	int start = 97, end = 122, count = 1;
+
+	while (count <= 10)
+	{
+		while (start <= end)
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+
+		start = 97;
+		count++
+	}
+}
